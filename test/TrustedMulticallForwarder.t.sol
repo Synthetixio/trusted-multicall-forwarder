6989908db5bf4c3e46a6fe3d4b453acbdd96944d
JaredBorders <jlbxsxs@gmail.com>
2023-10-24 20:12:41 -0400

    ✅ Test invalid signature

--- test/TrustedMulticallForwarder.t.sol
+++ test/TrustedMulticallForwarder.t.sol
@@ -6,7 +6,8 @@ import {Vm} from "../lib/forge-std/src/Vm.sol";
 
 import {
     TrustedMulticallForwarder,
-    ERC2771Forwarder
+    ERC2771Forwarder,
+    Address
 } from "../src/TrustedMulticallForwarder.sol";
 
 contract ERC2771Example {
@@ -26,7 +27,9 @@ contract TrustedMulticallForwarderTest is Test {
 
     // actor(s)
     address internal signer;
-    uint256 internal signerPrivateKey = 0x12341234;
+    uint256 internal signerPrivateKey = 0x1;
+    address internal badSigner;
+    uint256 internal badSignerPrivateKey = 0x2;
 
     function setUp() public {
         // deploy contract(s)
@@ -35,6 +38,7 @@ contract TrustedMulticallForwarderTest is Test {
 
         // initialize actor specific state
         signer = vm.addr(signerPrivateKey);
+        badSigner = vm.addr(badSignerPrivateKey);
     }
 }
 
@@ -141,5 +145,57 @@ contract ExecuteBatch is TrustedMulticallForwarderTest {
         assertEq(results.length, 1);
         assertEq(results[0].success, true);
         assertEq(results[0].returnData, abi.encode("pong"));
+    }
+
+    function test_executeBatch_invalid_signature() public {
+        // tx details
+        uint256 value = 1 ether;
+        uint256 gas = 1 ether;
+
+        // prepare forward request data (with empty signature)
+        ERC2771Forwarder.ForwardRequestData memory request = ERC2771Forwarder
+            .ForwardRequestData({
+            from: address(signer),
+            to: address(erc2771Example),
+            value: value,
+            gas: gas,
+            deadline: type(uint48).max,
+            data: abi.encodeWithSelector(ERC2771Example.ping.selector),
+            signature: bytes("") // initially empty
+        });
+
+        // define domain separator
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                _TYPE_HASH,
+                keccak256(bytes("trusted-multicall-forwarder")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(trustedMulticallForwarder)
+            )
+        );
+
+        // sign forward request data as bad signer
+        bytes memory invalidSignature = getForwardRequestDataSignature(
+            request,
+            trustedMulticallForwarder.nonces(address(signer)),
+            badSignerPrivateKey, // bad signer
+            domainSeparator
+        );
+
+        // update forward request data object with signature
+        request.signature = invalidSignature;
+
+        // define batch of forward requests
+        ERC2771Forwarder.ForwardRequestData[] memory batch =
+            new ERC2771Forwarder.ForwardRequestData[](1);
+        batch[0] = request;
+
+        // execute batch
+        vm.expectRevert(Address.FailedInnerCall.selector);
+        TrustedMulticallForwarder.Result[] memory results =
+        trustedMulticallForwarder.executeBatch{value: value, gas: gas}({
+            requests: batch
+        });
     }
 }
